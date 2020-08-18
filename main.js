bd7668af31854f988d3c73c24387d72254a4de4c
kitilopemi <aiyegbusikiti@gmail.com>
2020-08-18 08:25:23 -0700

    reg form

---
+++ main.js
@@ -0,0 +1,13 @@
+function confam(){
+    confirm("are you sure ?")
+}
+function mal(){
+    cmail = null
+    var cmail = prompt("Enter your Email address for further contact")
+    if (cmail==="") {
+        prompt("please enter your Email address for further contact");
+    }
+     else{
+         alert("Thank you !");
+     }
+}
