010bc00dc95bd1da1ec7650183286f3cdd965548
kitilopemi <aiyegbusikiti@gmail.com>
2020-08-20 12:27:31 +0100

    form reg

--- README.md
+++
@@ -1,1 +0,0 @@
-# aiykiti.github.io
--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-function confam(){
-    confirm("are you sure ?")
-}
-function mal(){
-    cmail = null
-    var cmail = prompt("Enter your Email address for further contact")
-    if (cmail==="") {
-        prompt("please enter your Email address for further contact");
+
+
+const submitForm = document.querySelector("#submit");
+console.log(submitForm);
+const validate = (e) => {
+    e.preventDefault();
+    const name = document.querySelector("#name").value;
+    const number = document.querySelector("#number").value;
+    const address = document.querySelector("#address").value;
+    console.log(name, number, address);
+    if (!name.match(/^[A-Za-z][-a-zA-Z ]+$/)){
+        alert("Name must contain only alphabets");
+        return false;
     }
-     else{
-         alert("Thank you !");
-     }
+    else if (!number.match(/^(?=.*[0-9])[- +()0-9]+$/)){
+        alert("Enter a valid number");
+        return false;
+    }
+    else if (address===''){
+        alert("Address must not be empty");
+    }
+    else {
+        alert("You have succesfully registered !");
+    }
 }
+
+
+submitForm.addEventListener("click",validate)
+
